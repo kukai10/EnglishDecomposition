314454f1bcfd05c47bf399ee94d81bb90eb45843
kukaiN <kukainakahata@gmail.com>
2020-01-16 00:16:15 -0500

    Update eng_decomp.py

---
+++ eng_decomp.py
@@ -0,0 +1,41 @@
+# goals of this project:
+"""
+# - the program should be able to take formal and normal (text, speech, everyday conversation, etc) sentences as inputs
+# - find clauses that resides in sentences, then clasify them into types of clauses (main, subordinating, etc)
+#   as well as classify phrases and clauses that acts , as a whole, as a specific part od speach, ex: adjectival phrases 
+"""
+
+
+# part of sentences and related vocabs:
+"""
+- noun
+- verb
+- adjective
+- adverb
+- conjunction
+- 
+
+
+
+
+
+
+"""
+
+# thigs to look out for or errors to reconize:
+"""
+- subject-verb error
+
+
+
+
+"""
+
+# things required:
+
+
+
+
+
+
+
